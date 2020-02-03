d20f5fe0317510dadb442eea38926077e2d539db
Lessie06 <60441086+Lessie06@users.noreply.github.com>
2020-02-03 12:37:19 -0500

    Add files via upload

---
+++ practiceweek2.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ * @author Lessie
+ * created 2/3/2020
+ * description: variable practice
+ *
+ **/
+
+
+public class practiceweek2 {
+	public static void main(String[] args) {
+		// Define a variable with type double with a value of 10.35
+		double num1= 10.35;
+		// byte with value 5
+		byte num2=5;
+		//int with value 3
+		int num3=3;
+		
+		//Declare 3 string variables
+		 String name1;
+		 String name2;
+		 String name3;
+		
+		//Assign values to the previously created variables
+		name1= "John";
+		name2= "Mary";
+		name3= "Dog";
+		System.out.println(name1);
+		System.out.println(name1+ name2);
+		
+		
+	
+	}
+		
+}

dcccc49a816cd2542e77ff4e65c5ae2e14f89d55
Lessie06 <60441086+Lessie06@users.noreply.github.com>
2020-02-26 12:35:34 -0500

    Add files via upload

---
+++ CountingNumbers.java
@@ -0,0 +1,27 @@
+/**
+ * author:Lessie
+ * Description: counting numbers
+ * 
+ * 
+ */
+
+
+import java.util.Scanner;
+
+public class CountingNumbers {
+	public static void main(String[] args) {
+		Scanner keyboard=new Scanner(System.in);
+		for (int i=5; i<100; i++) {
+			System.out.println(i);
+		}
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+
+}
